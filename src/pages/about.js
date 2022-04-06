6398d46bd8e9936144ba8acfb0efff7fb4616e27
Favour Max-Oti <maxotif@gmail.com>
2022-04-06 11:05:44 +0100

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reiciendis aliquid, delectus placeat veniam similique necessitatibus adipisci eum perferendis nulla soluta! Adipisci maiores, officiis fuga dolores culpa laudantium libero accusantium.
+        Magni, recusandae doloribus. Expedita qui nisi doloremque ullam dignissimos consequatur officiis numquam fuga modi molestiae illum dolores, repudiandae facilis eos voluptatem architecto voluptates accusamus. Tenetur minus molestias hic deleniti repellat.
+        Dicta asperiores ad laborum tempore dignissimos commodi, voluptas animi aliquid, modi voluptatum distinctio excepturi? Asperiores autem alias esse iusto ipsa in, inventore itaque dolorem, dignissimos minima doloremque incidunt dolores sapiente!
+        Atque, doloribus illum, accusantium hic repellendus at vitae obcaecati nulla praesentium provident velit qui tempora aliquid! Eaque voluptatibus voluptas illum dignissimos fugiat labore laborum ut, eos totam sapiente, recusandae odio!
+        </p>
+    </div>
+  )
+}
+
+export default about
